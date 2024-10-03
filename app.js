141cf7f68b360f21d75e19eae965261963c40aa0
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2024-10-04 00:01:57 +0300

    mvp

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,5 +18,73 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
+const WebSocket = require('ws');
+const {json} = require("express");
+
+const meetChatWebSocketServer = new WebSocket.Server({port: 3003});
+
+const meet = {
+    users: [],
+    chatHistory : [],
+}
+const sendToAllChatUsers =  (payload) => {
+
+    const message =  JSON.stringify(payload)
+
+    meet.users.forEach(user => {
+        const {webSocketClient} = user;
+        // if (client.readyState === WebSocket.OPEN) {
+        // if (client.readyState === 'open') {
+        //     client.send(message);
+        // }
+        //
+        webSocketClient.send(message)
+    });
+}
+
+meetChatWebSocketServer.on('connection', function connection(ws,req) {
+    // TODO из req вытащить юзера
+
+    meet.chatHistory.forEach((msg)=>{
+        ws.send( JSON.stringify(msg) )
+    })
+
+
+    sendToAllChatUsers( {user_id : meet.users.length ,text:'ворвался в чат'} )
+
+
+
+    meet.users.push({
+        user_id: meet.users.length,
+        webSocketClient:ws
+    });
+
+    ws.on('message', (payload) => {
+
+        const message = JSON.parse(payload)
+        meet.chatHistory.push(message)
+
+        sendToAllChatUsers(message)
+
+    });
+
+    ws.on('close', () => {
+        // Remove the client from the array when it disconnects
+        const index = meet.users.indexOf(ws);
+
+        if (!index > -1) {
+           return
+        }
+        const deletedUser =  meet.users[index]
+
+        sendToAllChatUsers( { user_id: deletedUser.user_id,text:'покинул чат'} )
+
+        meet.users.splice(index, 1);
+
+
+    });
+
+
+});
 
 module.exports = app;
