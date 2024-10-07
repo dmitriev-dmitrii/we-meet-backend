7c7db4742b17138b205072ef7c9a0d420a5b2f76
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2024-10-07 23:34:28 +0300

    add channels draft

--- app.js
+++ app.js
@@ -27,8 +27,57 @@ var server = http.createServer(app);
 
 const meetChatWebSocketServer = new WebSocket.Server({server});
 
+const channels = new Map();
+
+function joinChannel(channel, ws) {
+  if (!channels.has(channel)) {
+    channels.set(channel, new Set());
+  }
+  channels.get(channel).add(ws);
+}
+
+function leaveChannel(channel, ws) {
+  if (channels.has(channel)) {
+    channels.get(channel).delete(ws);
+    if (channels.get(channel).size === 0) {
+      channels.delete(channel);
+    }
+  }
+}
+
 meetChatWebSocketServer.on('connection', function connection(ws,req) {
     console.log('EPTA')
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        
+        switch(data.type) {
+            case 'join':
+                joinChannel(data.channel, ws);
+                break;
+            case 'leave':
+                leaveChannel(data.channel, ws);
+                break;
+            case 'message':
+                if (channels.has(data.channel)) {
+                    channels.get(data.channel).forEach((client) => {
+                        if (client !== ws && client.readyState === WebSocket.OPEN) {
+                            client.send(JSON.stringify({
+                                type: 'message',
+                                channel: data.channel,
+                                content: data.content
+                            }));
+                        }
+                    });
+                }
+                break;
+        }
+    });
+
+    ws.on('close', () => {
+        channels.forEach((clients, channel) => {
+            leaveChannel(channel, ws);
+        });
+    });
 })
 
 server.listen(port);
