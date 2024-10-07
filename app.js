d421231366112a0a2f07846ee2bacd6c7b1acb80
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2024-10-07 23:30:04 +0300

    delete bin

--- app.js
+++ app.js
@@ -17,74 +17,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const http = require('http');
 
 const WebSocket = require('ws');
-const {json} = require("express");
 
-const meetChatWebSocketServer = new WebSocket.Server({port: 3003});
+var port = process.env.PORT || '3000';
+app.set('port', port);
+var server = http.createServer(app);
 
-const meet = {
-    users: [],
-    chatHistory : [],
-}
-const sendToAllChatUsers =  (payload) => {
-
-    const message =  JSON.stringify(payload)
-
-    meet.users.forEach(user => {
-        const {webSocketClient} = user;
-        // if (client.readyState === WebSocket.OPEN) {
-        // if (client.readyState === 'open') {
-        //     client.send(message);
-        // }
-        //
-        webSocketClient.send(message)
-    });
-}
+const meetChatWebSocketServer = new WebSocket.Server({server});
 
 meetChatWebSocketServer.on('connection', function connection(ws,req) {
-    // TODO из req вытащить юзера
+    console.log('EPTA')
+})
 
-    meet.chatHistory.forEach((msg)=>{
-        ws.send( JSON.stringify(msg) )
-    })
-
-
-    sendToAllChatUsers( {user_id : meet.users.length ,text:'ворвался в чат'} )
-
-
-
-    meet.users.push({
-        user_id: meet.users.length,
-        webSocketClient:ws
-    });
-
-    ws.on('message', (payload) => {
-
-        const message = JSON.parse(payload)
-        meet.chatHistory.push(message)
-
-        sendToAllChatUsers(message)
-
-    });
-
-    ws.on('close', () => {
-        // Remove the client from the array when it disconnects
-        const index = meet.users.indexOf(ws);
-
-        if (!index > -1) {
-           return
-        }
-        const deletedUser =  meet.users[index]
-
-        sendToAllChatUsers( { user_id: deletedUser.user_id,text:'покинул чат'} )
-
-        meet.users.splice(index, 1);
-
-
-    });
-
-
-});
-
-module.exports = app;
+server.listen(port);
+console.log(`app listed http://localhost:${port}`)
