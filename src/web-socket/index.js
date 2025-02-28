266317833af7eef6b1fbfad69ba75703409d37f4
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2025-02-28 20:18:15 +0300

    send-ws-messages-only-meet

--- src/web-socket/index.js
+++ src/web-socket/index.js
@@ -1,80 +1,92 @@
-import { WebSocket } from 'ws';
+import {WebSocket} from 'ws';
 
 import {meetService} from "../services/meet/meetService.js";
 import {usersService} from "../services/user/usersService.js";
 
+const WS_EVENTS = {
+    UPDATE_MEET_ONLINE_USERS: 'update-meet-online-users'
+}
 
-async  function  onSocketConnect ( ws , { url  , headers } )  {
+async function onSocketConnect(ws, {url, headers}) {
 
-        const params = new URL(url, `https://${headers.host}`)
+    const params = new URL(url, `https://${headers.host}`)
 
-        ws._userId =  params.searchParams.get('userId')
-        ws._meetId =  params.searchParams.get('meetId')
+    ws._userId = params.searchParams.get('userId')
+    ws._meetId = params.searchParams.get('meetId')
 
-        ws._user = await usersService.findUserById( ws._userId )
-        // ws._user = await usersService.findUserById( ws._userId )
+    ws._user = await usersService.findUserById(ws._userId)
+    // ws._user = await usersService.findUserById( ws._userId )
 
-        if ( !ws._user || !ws._meetId ) {
-            ws.close(3000)
+    if (!ws._user || !ws._meetId) {
+        ws.close(3000)
+    }
+
+    const meetWsClients = Array.from(this.webSocketServer.clients).filter((client) => {
+        return client._meetId === ws._meetId
+    });
+
+    const payload = {
+        from: 'wss',
+        type: WS_EVENTS.UPDATE_MEET_ONLINE_USERS,
+        data: {
+            meetOnlineUsers: meetWsClients.length,
+        }
+    }
+
+    meetWsClients.clients.forEach((client) => {
+        client.send(JSON.stringify(payload));
+    });
+
+
+    ws.on('message', (payload) => {
+
+        let data = JSON.parse(payload);
+        data.from = ws._userId;
+        data.user = ws._user;
+
+        if (data.to) {
+            const targetWsUser = [...this.webSocketServer.clients].find((item) => item._userId === data.to);
+            delete data.to;
+
+            if (targetWsUser && targetWsUser.readyState === WebSocket.OPEN) {
+                targetWsUser.send(JSON.stringify(data));
+            }
+            return;
         }
 
+        data = JSON.stringify(data)
+
+        this.webSocketServer.clients.forEach((client) => {
+            if (client !== ws && client._meetId === ws._meetId && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+
+        const meetWsClients = Array.from(this.webSocketServer.clients).filter((client) => {
+            return client._meetId === ws._meetId
+        });
+
         const payload = {
-            from:'wss',
-            type:'ws-connection',
-            data : {
-                wsClientsOnline: Array.from(this.webSocketServer.clients).map(( {_userId} )=> _userId).sort() ,
+            from: 'wss',
+            type: WS_EVENTS.UPDATE_MEET_ONLINE_USERS,
+            data: {
+                meetOnlineUsers: meetWsClients.length,
             }
         }
 
-        this.webSocketServer.clients.forEach((client) => {
+
+        meetWsClients.clients.forEach((client) => {
             client.send(JSON.stringify(payload));
         });
 
-
-        ws.on('message', (payload) => {
-
-            let data = JSON.parse(payload);
-            data.from = ws._userId;
-            data.user = ws._user;
-
-            if (data.to) {
-                const targetWsUser = [...this.webSocketServer.clients].find((item) => item._userId === data.to);
-                delete data.to;
-
-                if (targetWsUser && targetWsUser.readyState === WebSocket.OPEN) {
-                    targetWsUser.send(JSON.stringify(data));
-                }
-                return;
-            }
-
-            data = JSON.stringify(data)
-
-            this.webSocketServer.clients.forEach((client) => {
-                if (client !== ws && client.readyState === WebSocket.OPEN) {
-                    client.send(data);
-                }
-            });
-        });
-
-        ws.on('close', () => {
-
-            const payload = {
-                from:'wss',
-                type:'ws-close',
-                data : {
-                    wsClientsOnline:  Array.from(this.webSocketServer.clients).map(( {_userId} )=> _userId),
-                }
-            }
-
-            this.webSocketServer.clients.forEach((client) => {
-                client.send(JSON.stringify(payload));
-            });
-
-        });
+    });
 
 
 }
 
-export const  setupWebSocket = (webSocketServer)=> {
-    webSocketServer.on('connection', onSocketConnect.bind({webSocketServer}) );
+export const setupWebSocket = (webSocketServer) => {
+    webSocketServer.on('connection', onSocketConnect.bind({webSocketServer}));
 }
