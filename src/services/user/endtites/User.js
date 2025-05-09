67cdd44de7165a4d9ba0be812711df1e738e6d38
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2025-05-09 04:22:33 +0300

    upd userName

--- src/services/user/endtites/User.js
+++ src/services/user/endtites/User.js
@@ -10,7 +10,7 @@ export class User {
 
         const {useragent} = components
 
-        this.userName = userName ? userName : useragent.os.family + useragent.browser.family
+        this.userName = userName ? userName :`${ useragent.os.family } ${useragent.browser.family}`
         this.userAccentColor = '' //todo uniq accent color
 
         this.userId = `[${useragent.os.family}][${useragent.browser.family}-${useragent.browser.version}][${hash}]`.trim().toLowerCase().replace(/ /g, '-');
