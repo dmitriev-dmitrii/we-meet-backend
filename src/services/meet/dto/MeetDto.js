2c2153dc332cc150b747f49cfb49a4cee528af7b
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2025-08-02 00:39:37 +0300

    isPrivateMeet field

--- src/services/meet/dto/MeetDto.js
+++ src/services/meet/dto/MeetDto.js
@@ -2,10 +2,13 @@ export class MeetDto {
     meetId  = ''
     // meetUsers = []
     ownerUserId = ''
+    isPrivateMeet = false
 
-    constructor({ meetUsers , meetId , ownerUserId }) {
+    constructor({ meetUsers , meetId , ownerUserId , isPrivateMeet}) {
         this.meetId = meetId
         // this.meetUsers =  Array.from(meetUsers.values());
         this.ownerUserId = ownerUserId
+        this.isPrivateMeet = isPrivateMeet
+        // this.meetUsers = meetUsers
     }
 }
