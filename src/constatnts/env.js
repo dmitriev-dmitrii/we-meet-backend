9562d7285be4695aa566519072dcb955cc002f72
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2025-09-21 18:10:17 +0300

    update env

--- src/constatnts/env.js
+++ src/constatnts/env.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const IS_PROD_MODE =  process.env.MODE  === 'production';
-const IS_DEV_MODE = !IS_PROD_MODE;
+const IS_DEV_MODE = process.env.MODE  === 'dev';
+const IS_PROD_MODE =  !IS_DEV_MODE;
 
 export const env = {
     ...process.env,
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,15 +4,44 @@ import {env} from "../constatnts/env.js";
 import freeIce from "freeice";
 import {UserDto} from "../services/user/dto/UserDto.js";
 
+const {METERED_API_KEY,
+    IS_DEV_MODE,
+    TURN_USERNAME,
+    TURN_PASSWORD,
+    TURN_STATIC_AUTH_SECRET,
+    TURN_REALM,
+    TURN_PORT_TCP,
+    TURN_PUBLIC_IP,
+    TURN_PORT_UDP,
+    TURN_PORT_TLS,
+} = env
+
 const GOOGLE_ICES = [
     {urls: "stun:stun.l.google.com:19302"}, // Обязательно для Firefox
     {urls: "stun:stun1.l.google.com:19302"},
     {urls: "stun:stun2.l.google.com:19302"},
     {urls: "stun:stun3.l.google.com:19302"}
 ]
+
+const iceServers = [
+    {
+        urls: [
+            `stun:${TURN_PUBLIC_IP}:${TURN_PORT_UDP}`,
+            `stun:${TURN_PUBLIC_IP}:${TURN_PORT_TCP}`
+        ]
+    },
+    {
+        urls: [
+            `turn:${TURN_PUBLIC_IP}:${TURN_PORT_UDP}?transport=udp`,
+            `turn:${TURN_PUBLIC_IP}:${TURN_PORT_TCP}?transport=tcp`,
+            `turns:${TURN_PUBLIC_IP}:${TURN_PORT_TLS}?transport=tcp`
+        ],
+        username: TURN_USERNAME,
+        credential: TURN_PASSWORD
+    }
+];
 export const usersRouter = Router()
 
-const {METERED_API_KEY, IS_DEV_MODE} = env
 usersRouter.post('/auth', async ({body = {}, fingerprint}, res) => {
 
     const {userName = ''} = body
@@ -24,30 +53,19 @@ usersRouter.post('/auth', async ({body = {}, fingerprint}, res) => {
 
 usersRouter.get('/ice-servers', async (req, res) => {
 
-    const freeIces = [...GOOGLE_ICES, ...freeIce()]
-
     if (IS_DEV_MODE) {
-        res.send(freeIces)
+        res.send(iceServers)
         return
     }
 
-    const iceServers = [
-        {
-            urls: [
-                `turn:${process.env.TURN_PUBLIC_IP}:${process.env.TURN_PORT_UDP}`,
-                `turns:${process.env.TURN_PUBLIC_IP}:${process.env.TURN_PORT_TLS}`
-            ],
-            username: process.env.TURN_USERNAME,
-            credential: process.env.TURN_PASSWORD
-        }
-    ];
+    try {
+        const metredApiRes = await fetch(`https://we_meet.metered.live/api/v1/turn/credentials?apiKey=${METERED_API_KEY}`)
 
-    res.send(iceServers)
+        const metredIceServers = await metredApiRes.json();
+        res.send([...iceServers,...metredIceServers, ...freeIce()])
+    } catch (e) {
 
-    const metredApiRes = await fetch(`https://we_meet.metered.live/api/v1/turn/credentials?apiKey=${METERED_API_KEY}`)
+    }
 
-    const metredIceServers = await metredApiRes.json();
-
-    res.send([...metredIceServers, ...freeIces])
 })
 
