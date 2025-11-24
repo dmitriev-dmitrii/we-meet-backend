ea6ccd6173534cf6a687637064a700a8197aa90d
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2025-11-24 04:30:29 +0300

    /health

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import {meetRouter} from "./meet.js";
 import {usersRouter} from "./users.js";
+import {constants} from "http2";
 
 const API_VERSION_PREFIX = '/api'
 export const  setupRoutes = (app)=> {
+  app.use(`${API_VERSION_PREFIX}/health`, (req, res) => res.sendStatus(constants.HTTP_STATUS_OK));
   app.use(`${API_VERSION_PREFIX}/meet`, meetRouter);
   app.use(`${API_VERSION_PREFIX}/users`, usersRouter);
 }
